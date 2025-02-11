121a157b9f475982164368ce5aa52a26eaca0379
root <root@Anjali.>
2025-02-11 17:50:34 +0000

    Add a file and import modules

---
+++ voice_assistant.py
@@ -0,0 +1,5 @@
+import pyttsx3 # text to speech conversion
+import speech_recognition as sr # speech recognition
+import webbrowser # web browser
+import datetime # date and time
+import pyjokes # generates prgramming related jokes
