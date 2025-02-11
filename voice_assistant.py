c38a0ce7c8184840942bfb5941a5c83a7ff1a7d2
ANJALI VERMA <143899915+anjali067verma@users.noreply.github.com>
2025-02-12 00:14:59 +0530

    Added new function in voice_assistant

--- voice_assistant.py
+++ voice_assistant.py
@@ -4,6 +4,7 @@ import webbrowser # web browser
 import datetime # date and time
 import pyjokes # generates prgramming related jokes
 
+#Speech to text
 def sptext():
     recognize=sr.Recognizer()
     with sr.Microphone()as source:
@@ -14,6 +15,21 @@ def sptext():
             print("Recognizing...")
             data=recognize.recognize_google(audio)
             print(data)
+            return data
         except sr.UnknownValueError:    
-            print("Not recognized")
-sptext()            
+            print("Not recognized")          
+
+#Text to speech
+def texttosp(x):
+    engine=pyttsx3.init()
+    voices=engine.getProperty('voices')
+    engine.setProperty('voice',voices[1].id)# voices[0] for male voice and vioces[1] for female voice
+    rate=engine.getProperty('rate')
+    engine.setProperty('rate',105)
+    engine.say(x)
+    engine.runAndWait()
+
+if __name__=='__main__':
+     
+     if sptext().lower()=="hey anjali":
+         print("Test")
