40f61d5eb5ff98abc9f21b22ba6d87da6971ef10
ANJALI VERMA <143899915+anjali067verma@users.noreply.github.com>
2025-02-11 23:43:45 +0530

    Updated voice_assistant.py

--- voice_assistant.py
+++ voice_assistant.py
@@ -3,3 +3,17 @@ import speech_recognition as sr # speech recognition
 import webbrowser # web browser
 import datetime # date and time
 import pyjokes # generates prgramming related jokes
+
+def sptext():
+    recognize=sr.Recognizer()
+    with sr.Microphone()as source:
+        print("Listening...")
+        recognize.adjust_for_ambient_noise(source) # remove noise
+        audio=recognize.listen(source)
+        try:
+            print("Recognizing...")
+            data=recognize.recognize_google(audio)
+            print(data)
+        except sr.UnknownValueError:    
+            print("Not recognized")
+sptext()            
